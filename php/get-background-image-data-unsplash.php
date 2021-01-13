5a8fd229177e6aea8f20952e5f8460cace4ca0bd
Declan Stewart <declan_stewart@yahoo.com>
2021-01-13 11:31:59 +0000

    adding Data file and seasonal background image update

--- php/get-background-image-data-unsplash.php
+++ php/get-background-image-data-unsplash.php
@@ -3,7 +3,24 @@
 $includekeys =__DIR__ . '/../../../keys.php';
 require_once ( $includekeys );
 
-$url = UNSPLASH_API_URL.'/photos/random?orientation=portrait&query=nature&client_id='.UNSPLASH_API_KEY;
+function timestampToSeason($dateTime){
+    $dayOfTheYear = date("z",strtotime($dateTime));
+    //$dayOfTheYear = $dateTime->format('z');
+    if($dayOfTheYear < 78 || $dayOfTheYear > 353){
+        return 'Winter';
+    }
+    if($dayOfTheYear < 170){
+        return 'Spring';
+    }
+    if($dayOfTheYear < 263){
+        return 'Summer';
+    }
+    return 'Fall';
+}
+
+$season = timestampToSeason(date("Y-m-d"));
+
+$url = UNSPLASH_API_URL.'/photos/random?orientation=portrait&query=nature,'.$season.'&client_id='.UNSPLASH_API_KEY;
 $response = file_get_contents($url);
 $response = json_encode($response);
 
